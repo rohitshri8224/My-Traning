79898ac2e79c1c0294b7f562554188035395f92f
Rohitshri8224 <rohitshrivastava8224@gmail.com>
2022-08-15 19:43:21 +0530

    session/mongo-session1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rohitshri8224:<password>@cluster0.asujdxx.mongodb.net/RohitShrivastava", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
