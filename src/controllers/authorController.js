8d735c9b14a9aa94e73487cc20ed91a9ece3fe50
Satish <satish_95raj@outlook.com>
2022-09-06 14:09:08 +0530

    done

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -9,16 +9,16 @@ const createAuthor = async function(req,res){
      return res.status(400).send({staus:true, error:'Invalid Email Id'})
 
      //First name validation
-     if (!/^[a-zA-Z.]{5,10}$/.test(data.fname))
+     if (!(/^[a-zA-Z.]{5,10}$/).test(data.fname))
      return res.status(400).send({staus:true, error:'Only alphabets !!'})
 
      //Last name validation
-     if (!/^[a-zA-Z.]{5,10}$/.test(data.lname))
+     if (!(/^[a-zA-Z.]{5,10}$/).test(data.lname))
      return res.status(400).send({staus:true, error:'Only alphabets !!'})
     
      //password validation
-     if(/^?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(data.password))
-     return res.status(400).send({status:false, error:'Length should be 8 or more!'})
+     if(!(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/).test(data.password))
+     return res.status(400).send({status:false, error:'Atleat 1 capital, 1 small, numbers and Length should be 8 or more!'})
     
     let savedData = await authorModel.create(data)
     res.status(201).send({msg:savedData})
