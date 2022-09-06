ab7a2694cc43274b7ad4b120bc35c36a35c3870b
Satish <satish_95raj@outlook.com>
2022-09-06 07:21:46 +0530

    organised the blog model

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -2,18 +2,29 @@ const mongoose = require('mongoose')
 const objectId= mongoose.Schema.Types.ObjectId 
 const blogSchema = new mongoose.Schema({
 
-title: {type: String, required : true},
- body: {type:String, required: true }, 
- authorId: {type:objectId, required: true ,ref:"Author" }, 
+title: {
+    type: String, 
+    required : true},
+ body: {
+    type:String, 
+    required: true 
+}, 
+ authorId: {
+    type:objectId, 
+    required: true,
+    ref:"Author" 
+}, 
  tags: [String],
- category: {type: String, required:true}, 
-    subcategory: [String],
-    isDeleted: {
-        type: Boolean,
-        default: false
+ category: {
+    type: String, 
+    required:true
+}, 
+subcategory: [String],
+isDeleted: {
+    type: Boolean,
+    default: false
     },
-
-    isPublished : {
+isPublished : {
         type: Boolean,
         default: false
     }
