7205a2612333549a33ea274362e362c66c0ed04e
Satish <satish_95raj@outlook.com>
2022-09-06 07:03:26 +0530

    Update code for create blog

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,55 +1,45 @@
-const blogModel = require("../models/blogModel")
-const authorModel = require('../models/authorModel')
+const blogModel = require("../models/blogModel");
+const authorModel = require("../models/authorModel");
 
-const getBlogs = async function(req,res){
-
-   try{ const query = req.query
-    const temp ={isDeleted:false, isPublished:true}
+const getBlogs = async function (req, res) {
+  try {
+    const query = req.query;
+    const temp = { isDeleted: false, isPublished: true };
     //console.log()
-    const final = Object.assign({},query,temp)
-    
+    const final = Object.assign({}, query, temp);
 
-    if(Object.keys(query).length == 0)
-    {
-        res.status(404).send({status:false, msg:"no such data"})
+    if (Object.keys(query).length == 0) {
+      res.status(404).send({ status: false, msg: "no such data" });
+    } else {
+      let data = await blogModel.find(final);
+      if (data.length == 0)
+        res.status(404).send({ status: false, msg: "no such data" });
+      else res.status(200).send({ status: true, data: data });
     }
-    else
-    {
-        let data = await blogModel.find(final)
-        if(data.length==0)
-        res.status(404).send({status:false, msg:"no such data"})
-        else
-        res.status(200).send({status:true, data:data})
-
-
-    }}catch(err)
-    {
-        res.status(500).send({status:false,msg:err.message})
-    }
-}
+  } catch (err) {
+    res.status(500).send({ status: false, msg: err.message });
+  }
+};
 
 //create post api
 
-const createBlog = async function(req,res){
-    try{
-    let blog = req.body
-    let authorId = blog.authorId
-    if(authorId){
-        let author_id = await authorModel.findById(authorId)
-        if(author_id){
-            let createBlogs =  await blogModel.create(blog)
-            res.status(201).send({status:true, data:createBlogs})
-        }else{
-            res.status(400).send({status:false,msg:'Invalid Author Id !'})
-        }
-    }else{
-        res.status(404).send({status:false,msg:'Invalid Author Id !'})
-    }
-}catch(err){
-    res.status(500).send({error:err.message})
-}
-}
+const createBlog = async function (req, res) {
+  try {
+    let blog = req.body;
+    let authorId = blog.authorId;
+    if (!authorId)
+      return res.status(404).send({ status: false, msg: "Required Author Id !" });
 
+    let author_id = await authorModel.findById(authorId);
+    if (!author_id)
+      return res.status(400).send({ status: false, msg: "Invalid Author Id !" });
 
-module.exports.createBlog=createBlog
-module.exports.getBlogs = getBlogs
+    let createBlogs = await blogModel.create(blog);
+     res.status(201).send({ status: true, data: createBlogs });
+  }  catch (err) {
+      res.status(500).send({ error: err.message });
+  }
+};
+
+module.exports.createBlog = createBlog;
+module.exports.getBlogs = getBlogs;
